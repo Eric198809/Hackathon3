e3cfec391aabb9d10dc0a8210d4c1b8d84ebe21b
Ioana Horeanu <ioana.catalina10@yahoo.com>
2024-02-29 19:16:54 +0100

    ajout credits

---
+++ Assets/Script/LoadCredits.cs
@@ -0,0 +1,17 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadCredits : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+  
+     if(collision.CompareTag("Player"))
+       {
+                SceneManager.LoadScene("fin");
+       }  
+    }
+
+}
